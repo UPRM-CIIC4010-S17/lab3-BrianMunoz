3035069d40d0ac17ef9ca5aa76b66d9aa947a58a
4010d3 <4010d3@tamarindo.ece.uprm.edu>
2017-02-08 15:02:48 -0400

    Importing the classes of Dimension and ToolKit. Change title to "Brian" and set location on the center of the screen.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,18 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
+		
+		JFrame myframe = new JFrame("Brian");
+		myframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		screenSize.getHeight();
+		screenSize.getWidth();
+		
+		myframe.setLocation(screenSize.width/2,screenSize.height/2);
+		myframe.setSize(200, 200);
+		myframe.setVisible(true);
 	}
 }
